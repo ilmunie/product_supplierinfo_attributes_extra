0917a2ef98375fe900da4f4a868e646a0aa05aac
sapel <santiago.apel@eynes.com.ar>
2023-07-25 23:20:35 -0300

    [FIX] domain attributes

--- models/supplier_variant_info.py
+++ models/supplier_variant_info.py
@@ -3,9 +3,9 @@ from odoo import models, fields, api
 class ProductSupplierInfoVarianInfo(models.Model):
     _name = 'supplier.variant.info'
 
-    attribute_ids = fields.Many2many(comodel_name='product.template.attribute.value', string="Atributos")
     extra_amount = fields.Float(string="Valor extra")
     pricelist_id = fields.Many2one('product.supplierinfo')
+    attribute_ids = fields.Many2many(comodel_name='product.template.attribute.value', string="Atributos")
     product_template_id = fields.Many2one(related='pricelist_id.product_tmpl_id',store=True,readonly=False)
 
     def name_get(self):
@@ -24,10 +24,12 @@ class ProductSupplierInfoVarianInfo(models.Model):
     def _onchange_pricelist_product_tmpl(self):
         for rec in self:
             if rec.pricelist_id and rec.pricelist_id.product_tmpl_id:
-                #import pdb;pdb.set_trace()
                 ids = []
-                for attribute_line in rec.pricelist_id.product_tmpl_id.attribute_line_ids:
-                    ids.extend(attribute_line.value_ids.mapped('id'))
+                for pp in rec.pricelist_id.product_tmpl_id.product_variant_ids:
+
+                    #import pdb;
+                        #pdb.set_trace()
+                    ids.extend(pp.product_template_variant_value_ids.mapped('id'))
                 return {'domain':{'attribute_ids': [('id','in',ids)]}}
             else:
                 return
